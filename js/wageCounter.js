1c6c95cd89ef76beb17ccc139d8c3079d571ffbc
spiikki sillanpää <spiikki@nalleperhe.net>
2017-03-11 01:42:23 +0200

    wages calculated and results rendered. a bit of kekkonen but i got carried away

--- js/wageCounter.js
+++ js/wageCounter.js
@@ -31,9 +31,11 @@ function parseFile(event) {
 		// parse data to objects
 		var data = $.csv.toObjects(event.target.result);
 
-		// parse data to renderable log, and store it in session
+		// parse data to renderable log
 		var log = parseLog(data);
-		sessionStorage.worklog = JSON.stringify(log);
+
+		// calculate wages into log
+		log = calculateWages(log);
 
 		// send to renderer
 		renderLog(log);
@@ -52,14 +54,16 @@ function parseLog(data) {
 
 	workLog = {
 		userID : {
-			username : "string",
+			username : string,
 			log : {
 				date : {
 					hours: float,
 					evening_extra: float,
-					overtime_extra: float
+					overtime_extra: float,
+					wage: float // daily wage
 				}
-			}
+			},
+			wage: float // monthly wage
 		}
 	}
 	*/
@@ -72,12 +76,12 @@ function parseLog(data) {
 
 		// check if there is user in log, if not, create it
 		if(workLog[row["Person ID"]] === undefined) {
-			workLog[row["Person ID"]] = { username: row["Person Name"], log: {} };
+			workLog[row["Person ID"]] = { username: row["Person Name"], log: {}, wage: 0.0 };
 		}
 
-		// check if there is date in log, if not, create it
+		// check if there is date in users log, if not, create it
 		if(workLog[row["Person ID"]].log[row["Date"]] === undefined) {
-			workLog[row["Person ID"]].log[row["Date"]] = { hours: 0.0, evening_extra: 0.0, overtime_extra: 0.0 };
+			workLog[row["Person ID"]].log[row["Date"]] = { hours: 0.0, evening_extra: 0.0, overtime_extra: 0.0, wage: 0.0 };
 		} 
 
 		// count hours on row
@@ -169,6 +173,87 @@ function parseLog(data) {
 
 }
 
+function calculateWages(log) {
+
+	// fetch values from settings
+	var wages = {
+		hourly : parseFloat($('input[name=wage_hourly').val()),
+		evening : parseFloat($('input[name=wage_evening').val())
+	};
+
+	$.each(log, function(index, user) {
+
+		$.each(user.log, function(index, logDate) {
+			var basicWage = logDate.hours * wages.hourly;
+			var evening = logDate.evening_extra * wages.evening;
+			var overtime = calculateOvertimeBonus(logDate.overtime_extra, wages, 0.25);
+
+			logDate.wage = basicWage + evening + overtime;
+
+			// add to monthly wage
+			user.wage += logDate.wage;
+		});
+
+	});
+
+	return log;
+}
+
+// holy recursion, coderman!
+function calculateOvertimeBonus(hours, wages, raise) {
+	if(raise > 1) raise = 1.00;
+
+	if( hours <= 2 ) {
+		return hours * (wages.hourly * raise);
+	} else {
+		return 2*(wages.hourly * raise) + calculateOvertimeBonus(hours-2, wages, raise*2);
+	}
+
+}
+
 function renderLog(log) {
 	console.log(log);
+
+	// setup html to be injected to DOM
+	var html = "";
+
+	// let's do that looping users thing again!
+	$.each(log, function(index, user) {
+
+		html += '<div class="user">';
+		html += '<h3>'+user.username+'</h3>';
+		html += '<h4> Monthly wage: $' + user.wage + '</h4>';
+
+		html += '<a href="#" name="toggleDetails">Toggle details</a>';
+
+		html += '<div class="details" style="display: none">';
+		// render details
+		$.each(user.log, function(index, logDate) {
+			html += '<h5>'+index+'</h5>';
+			html += '<p>';
+			html += 'Hours: ' + logDate.hours + '<br />';			
+			html += 'Evening hours: ' + logDate.evening_extra + '<br />';			
+			html += 'Overtime hours: ' + logDate.overtime_extra + '<br />';			
+			html += 'Daily wage: $' + logDate.wage + '<br />';			
+			html += '</p>';
+		});
+		html += '</div>';
+
+		html += '</div>';
+
+	});
+
+	// inject results!
+	$('section[name=results]').html(html);
+
+	// handle toggling visibility of details
+	$('a[name=toggleDetails]').click(function(event) {
+		if($(event.target).next().css('display') === 'none') {
+			$(event.target).next().css('display','block');
+		} else {
+			$(event.target).next().css('display','none');
+		}
+	});
+	
+
 }
