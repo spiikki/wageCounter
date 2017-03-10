366073190c45be7c8c1c0f85c22b33fb7014985d
spiikki sillanpää <spiikki@nalleperhe.net>
2017-03-10 15:36:03 +0200

    data loading and parsing

--- js/wageCounter.js
+++ js/wageCounter.js
@@ -1,16 +1,19 @@
 /*
 wageCounter-application
 (c) Petteri Spiikki Sillanpää / 2017 / spiikki@nalleperhe.net
-
-
 */
 
 function initializeWageCounter() {
 
 	// check the browser
 	if (window.File && window.FileReader && window.FileList && window.Blob) {
+
+		// setup variables
+		var hourLog = {};
+
 		// add listener to file-chooser
 		$('input[name=sourceFile]').change(parseFile);
+
 	} else {
 		$('section[name=application]').html('This application doesn\'t work with your browser. Sorry =(');
 	}
@@ -18,5 +21,21 @@ function initializeWageCounter() {
 }
 
 function parseFile(event) {
-	console.log(event.target.files);
+	console.log(event.target.files[0]);
+
+	var reader = new FileReader();
+
+	reader.onload = function(event) {
+		//console.log(event.target.result);
+
+		var data = $.csv.toObjects(event.target.result);
+
+		console.log(data);
+
+
+	};
+
+	reader.readAsText(event.target.files[0]);
+
+
 }
